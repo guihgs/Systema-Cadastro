3fbe4bbb2da354be35687a02421417826e90cc4a
Programação DGAZ <dgazprogramacao@gmail.com>
2019-01-16 13:05:00 -0200

    Sistema de Cadastro e login

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'cadastro');
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERRO: Não foi possivel se conectar. " . mysqli_connect_error());
+}
+?>
+
+
